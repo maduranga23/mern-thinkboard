2d14ef3cb6d769119d51597e4fd74db151768214
maduranga23 <maduranga.san2023@gmail.com>
2025-09-14 21:51:11 +0530

    Initial commit

---
+++ backend/src/routes/notesRoutes.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  createNote,
+  deleteNote,
+  updateNote,
+  getAllNotes,
+  getNoteById,
+} from "../controllers/notesController.js";
+
+const router = express.Router();
+
+router.get("/", getAllNotes);
+router.get("/:id", getNoteById);
+router.post("/", createNote);
+router.put("/:id", updateNote);
+router.delete("/:id", deleteNote);
+
+export default router;
+
+//endpoints
